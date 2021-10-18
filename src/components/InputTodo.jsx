5a70330f2dee974f2803d0102a54a77848e5e8c3
takamuu <mr.takamuu@gmail.com>
2021-10-18 18:09:43 +0900

    Create CompleteTodos IncompleteTodos

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -4,12 +4,12 @@ export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
     <div className="input-area">
-        <input 
-          placeholder="TODOを入力" 
-          value={todoText} 
-          onChange={onChange}
-        />
-        <button onClick={onClick}>追加</button>
-      </div>
+      <input 
+        placeholder="TODOを入力" 
+        value={todoText} 
+        onChange={onChange}
+      />
+      <button onClick={onClick}>追加</button>
+    </div>
   )
 }
